3e9375a3a82d6fbf47b513605b5c729c69af2fbd
lagunova1204 <lagunova.irina1204@gmail.com>
2022-11-30 17:17:21 +0300

    Задача 10

---
+++ task 10/Program.cs
@@ -0,0 +1,11 @@
+﻿int InputSecondNumber(int number)
+{
+    int count;
+    count = number % 100 / 10;
+    return count;
+}
+
+Console.WriteLine("Введите трехзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondNumber = InputSecondNumber(number);
+Console.WriteLine($"Вторая цифра {number} : {secondNumber}");
