1c055522fb2f670a306d133deea602c0c0afa970
lagunova1204 <lagunova.irina1204@gmail.com>
2022-11-30 18:03:33 +0300

    Задача 15

---
+++ task 15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Не выходной");
+        break;
+    case 2:
+        Console.WriteLine("Не выходной");
+        break;
+    case 3:
+        Console.WriteLine("Не выходной");
+        break;
+    case 4:
+        Console.WriteLine("Не выходной");
+        break;
+    case 5:
+        Console.WriteLine("Не выходной");
+        break;
+    case 6:
+        Console.WriteLine("Выходной");
+        break;
+    case 7:
+        Console.WriteLine("Выходной");
+        break;
+                           
+    default:
+        Console.WriteLine("Такого дня нет");
+        break;  
+}
